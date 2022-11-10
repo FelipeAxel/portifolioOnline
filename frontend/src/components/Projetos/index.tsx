9fb564967b8a2023e2ef44294d168d3e57841e7d
FelipeAxel <axelnovais96@gmail.com>
2022-11-10 18:07:43 -0300

    teste img 1

--- frontend/src/components/Projetos/index.tsx
+++ frontend/src/components/Projetos/index.tsx
@@ -1,12 +1,21 @@
+/* eslint-disable jsx-a11y/alt-text */
 import './style.css'
-
-
+import dsvendas from 'assets/img/dsvendas.jpeg'
 function Projetos() {
     return (
         <div className="teste5" >
             <div className="retirado">
                 <div className="primeiro">
-       
+                    <div className="col" >
+                        <div className="card shadow-sm">
+
+                        </div>
+                        <a href="https://axelnovais-dsmovie8.netlify.app">dsmovie8</a>
+                    </div>
+                    <div className="dsmovie">
+
+
+                    </div>
                     <div className="card-body">
                         <p>sadasd</p>
                     </div>
@@ -15,7 +24,7 @@ function Projetos() {
                 <div className="qwe">
                     <div className="col" >
                         <div className="card shadow-sm">
-                        
+                            <img className="teste7" src={dsvendas}></img>
                         </div>
                         <a href="https://axel-dsvendas.netlify.app">dsvendas</a>
                     </div>
@@ -28,9 +37,7 @@ function Projetos() {
                 <div className="zxc">
                     <div className="col" >
                         <div className="card shadow-sm">
-                           
-                            
-                            
+
                         </div>
                     </div>
                     <div className="card-body">
@@ -39,9 +46,6 @@ function Projetos() {
                 </div>
             </div>
         </div>
-
-
-
     );
 }
 
