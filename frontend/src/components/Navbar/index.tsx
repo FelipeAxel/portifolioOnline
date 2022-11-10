01825c23ab7e474fba636d284b19b62042efca9e
FelipeAxel <axelnovais96@gmail.com>
2022-11-10 16:14:31 -0300

    asd

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,5 +1,5 @@
 import { ReactComponent as GithubIcon } from 'assets/img/githubsvg.svg';
-import{ReactComponent as LinkedIn} from'assets/img/linkedin.svg'
+import { ReactComponent as LinkedIn } from 'assets/img/linkedin.svg'
 import './styles.css'
 
 function Navbar() {
@@ -9,7 +9,7 @@ function Navbar() {
                 <div className="portifolio-nav-content">
                     <h1>Meu Portifólio</h1>
                     <div className="btn projeto-btn">
-                    <a href="meuportifolioaxel.netlify.app/projetos">Projetos</a>
+                        <a href="meuportifolioaxel.netlify.app/projetos">Projetos</a>
                     </div>
                     <a href="https://github.com/FelipeAxel" target="_blank" rel="noreferrer">
                         <div className="portifolio-contact-container">
--- frontend/src/components/Projetos/index.tsx
+++ frontend/src/components/Projetos/index.tsx
@@ -3,50 +3,9 @@ import './style.css'
 
 function Projetos() {
     return (
-        <div className="teste5" >
-            <div className="retirado">
-                <div className="primeiro">
-                    <div className="col" >
-                        <div className="card shadow-sm">
-                        
-                        </div>
-                        <a href="https://axelnovais-dsmovie8.netlify.app">dsmovie8</a>
-                    </div>
-                    <div className="card-body">
-                        <p>sadasd</p>
-                    </div>
-                </div>
-
-                <div className="qwe">
-                    <div className="col" >
-                        <div className="card shadow-sm">
-                        
-                        </div>
-                        <a href="https://axel-dsvendas.netlify.app">dsvendas</a>
-                    </div>
-                    <div className="card-body">
-                        <p>sadasd</p>
-                    </div>
-                </div>
-
-
-                <div className="zxc">
-                    <div className="col" >
-                        <div className="card shadow-sm">
-                           
-                            
-                            
-                        </div>
-                    </div>
-                    <div className="card-body">
-                        <p>sadasd</p>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <p>asdsad</p>
         </div>
-
-
-
     );
 }
 
