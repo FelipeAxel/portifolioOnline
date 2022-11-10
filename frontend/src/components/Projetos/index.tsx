3fdd66141223b569e89a31739d33426d3cff73aa
FelipeAxel <axelnovais96@gmail.com>
2022-11-10 16:17:20 -0300

    hty

--- frontend/src/components/Projetos/index.tsx
+++ frontend/src/components/Projetos/index.tsx
@@ -3,9 +3,45 @@ import './style.css'
 
 function Projetos() {
     return (
-        <div>
-            <p>asdsad</p>
+        <div className="teste5" >
+            <div className="retirado">
+                <div className="primeiro">
+       
+                    <div className="card-body">
+                        <p>sadasd</p>
+                    </div>
+                </div>
+
+                <div className="qwe">
+                    <div className="col" >
+                        <div className="card shadow-sm">
+                        
+                        </div>
+                        <a href="https://axel-dsvendas.netlify.app">dsvendas</a>
+                    </div>
+                    <div className="card-body">
+                        <p>sadasd</p>
+                    </div>
+                </div>
+
+
+                <div className="zxc">
+                    <div className="col" >
+                        <div className="card shadow-sm">
+                           
+                            
+                            
+                        </div>
+                    </div>
+                    <div className="card-body">
+                        <p>sadasd</p>
+                    </div>
+                </div>
+            </div>
         </div>
+
+
+
     );
 }
 
