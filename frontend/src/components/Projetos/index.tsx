7f33a5a4e74c146fde01ef502825f895036932bb
FelipeAxel <axelnovais96@gmail.com>
2022-11-11 09:26:28 -0300

    correção ortografica

--- frontend/src/components/Projetos/index.tsx
+++ frontend/src/components/Projetos/index.tsx
@@ -16,9 +16,9 @@ function Projetos() {
                         <a href="https://axelnovais-dsmovie8.netlify.app">dsmovie8</a>
                     </div>
                     <div className="container">
-                        <p>Aplicativo desenvolvido na semana devsuperior com intuito de desenvolver a base de um programador fullstack, o dsmovie tem a funcionalidade de classificação do filme por estrelas
-                            Um forms para você poder por seu email e fazer sua avaliação.
-                            uma semana repleta de conhecimento e muita pratica.</p>
+                        <p>Aplicativo desenvolvido na semana devsuperior com intuito de desenvolver a base de um programador fullstack, o dsmovie tem a funcionalidade de classificação do filme por estrelas.
+                            Um forms para você poder por seu e-mail e fazer sua avaliação.
+                            Uma semana repleta de conhecimento e muita prática.</p>
                     </div>
                 </div>
 
@@ -30,7 +30,7 @@ function Projetos() {
                         <a href="https://axel-dsvendas.netlify.app">dsvendas</a>
                     </div>
                     <div className="container">
-                        <p>Aplicativo desenvolvido na semana devsuperior com intuito de desenvolver a base de um programador fullstack, o dsvendas tem a funcionalidade de listagem de vendas com um dashboard bem interativo, graficos e metricas, tudo sendo enviado por um banco de dados usando o postgresql.</p>
+                        <p>Aplicativo desenvolvido na semana devsuperior com intuito de desenvolver a base de um programador fullstack, o dsvendas tem a funcionalidade de listagem de vendas com um dashboard bem interativo, gráficos e métricas, tudo sendo enviado por um banco de dados usando o postgresql.</p>
                     </div>
                 </div>
             </div>
